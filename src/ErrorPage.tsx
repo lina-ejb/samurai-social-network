55cbe7293bd242e2bd23b37ba17dcc7213478b97
lina-ejb <linakryzneva@gmail.com>
2023-11-02 09:03:40 +0300

    Edit profile

--- src/ErrorPage.tsx
+++ src/ErrorPage.tsx
@@ -1,33 +1,40 @@
-import React from 'react';
-import { Box, Button, Container, Typography } from '@mui/material';
-import Grid from '@mui/material/Grid';
+import React from "react";
+import { Box, Button, Container, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import img from ".././src/assets/images/error.svg";
+import { useNavigate } from "react-router-dom";
 
 export default function ErrorPage() {
+  const navigate = useNavigate();
+  const navigateTo = () => {
+    navigate("/");
+  };
+
   return (
     <Box
       sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        minHeight: '100vh'
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh"
       }}
     >
       <Container maxWidth="md">
         <Grid container spacing={2}>
-          <Grid xs={6}>
+          <Grid xs={6} item>
             <Typography variant="h1">
               404
             </Typography>
             <Typography variant="h6">
               The page you’re looking for doesn’t exist.
             </Typography>
-            <Button variant="contained">Back Home</Button>
+            <Button variant="contained" onClick={navigateTo}>Back Home</Button>
           </Grid>
-          <Grid xs={6}>
+          <Grid xs={6} item>
             <img
-              src="https://cdn.pixabay.com/photo/2017/03/09/12/31/error-2129569__340.jpg"
+              src={img}
               alt=""
-              width={500} height={250}
+              width={600} height={600}
             />
           </Grid>
         </Grid>
